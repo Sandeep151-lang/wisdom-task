573b5071f685064f33826d1a583b996db8741bb6
Sandeep Nandanwar <sandeepnandanwar92@gmail.com>
2023-07-17 01:57:13 +0530

    fix pretteir code

--- src/components/Aggregate.jsx
+++ src/components/Aggregate.jsx
@@ -2,31 +2,22 @@ import React, { useState,useEffect } from 'react'
 import axios from 'axios'
 
 const Aggregate = () => {
-    
-
     const [branch1,setBranch1] = useState([])
     const [branch2,setBranch2] = useState([])
     const [branch3,setBranch3] = useState([])
     const [state,setState] = useState([])
-    
-    const [merged,setMerged] = useState([])
-const [searchQuery, setSearchQuery] = useState('');
+    const [merged,setMerged] = useState([]) 
+    const [searchQuery, setSearchQuery] = useState('');
 
     
 
     const getList = async()=>{
-    
-
     let endpoints = [
         'branch1.json',
         'branch2.json',
         'branch3.json'
       ];
-
       try {
-        
-     
-
     const res =  await  axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
     if(res){
         setBranch1(res[0]?.data?.products)
@@ -37,17 +28,18 @@ const [searchQuery, setSearchQuery] = useState('');
         console.log(error) 
 }
     }
-    useEffect(() => {
+
+useEffect(() => {
       getList()
     }, [])
     
+
 useEffect(()=>{
     setState([...branch1,...branch2,...branch3])
 },[branch1,branch2,branch3])
 
 
 useEffect(()=>{
-
  const mergedData = Object.values(state.reduce((acc, item) => {
     const key = item.id + item.name;
     const revenue = +(item?.unitPrice * item?.sold)
@@ -68,8 +60,6 @@ useEffect(()=>{
 //    const t= merged.reduce((a,i)=> a + i)
 //    console.log(t)
 //   }
-  
-
 },[state,searchQuery])
 
 
@@ -87,8 +77,6 @@ const handleSearch = event => {
   </tr>
   {
     merged.sort((a,b)=> a.name.localeCompare(b.name)).filter(item => item.name.toLowerCase().includes(searchQuery.toLowerCase())).map((item,key)=>{
-    
-       
         return (
             <>
                 <tr>
@@ -106,7 +94,6 @@ const handleSearch = event => {
     <th>Total Revenue</th>
     <th>{ 0}</th>
   </tr>
-
 </table>
    
     </>
